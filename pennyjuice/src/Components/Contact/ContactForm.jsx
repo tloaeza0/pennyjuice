ca98d198eb8b99e6338e48fbd026006993c6428c
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-05 15:43:58 -0400

    added form component and form page

---
+++ pennyjuice/src/Components/Contact/ContactForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const ContactForm = () => {
+  return (
+    <>
+      <form>
+        <lable>First Name</lable>
+        <input type="text" name="firstName"/>
+      </form>
+
+    </>
+  )
+}
+
+export default ContactForm
---
+++ pennyjuice/src/Components/Contact/contactPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContactForm from './ContactForm'
+
+
+const contactPage = () => {
+  return (
+    <div>
+      <ContactForm/>
+    </div>
+  )
+}
+
+export default contactPage
