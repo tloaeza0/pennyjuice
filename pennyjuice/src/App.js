6e275fde3837877eda6ecdadd48d9327fbc1389e
Dina Jarkas <djarkas98@gmail.com>
2022-04-06 15:02:30 -0400

    pushing homepage

--- pennyjuice/src/App.js
+++ pennyjuice/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+// import { Router, Route } from 'react-router-dom';
+
+import {
+  BrowserRouter as Router,
+  
+  Route,
+
+  Routes,
+}from "react-router-dom";
+
 
 function App() {
   return (
+    <>
+    <Router>
+
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Routes>
+     <Route path ="/" element = {<Home/>}></Route>
+
+   
+     </Routes>
+     
+      
     </div>
+    </Router>
+    </>
   );
 }
 
---
+++ pennyjuice/src/pages/Home.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+
+
+const Home = () => {
+return(
+   <>
+   
+   
+
+    <section id="hero">
+		<div class="hero-content">
+			<h1 class = "topstuff">100% blended fruit juice concentrate specifically designed for childcare centers. </h1>
+            <button class= "order" type="button">Order Now!</button>
+		</div>
+	</section>
+
+    
+        <section class="thegrid">
+        <section class="piece">
+          <h1 class="okay">RAINBOWS OF EXCITING FLAVORS!</h1>
+<div class = "no">
+        <article class = "pennn">15 flavors available including 7 color free flavors. Each case contains 6 - ½ gallon bottles of your choice. We’ve designed them especially for childcare, daycare, preschool, basp and extended daycare. Get in touch with us today!
+            <br/>
+            <br/>
+            <button class= "learn" type="button">Learn More!</button>
+            
+        </article>
+        
+        <img src="images/rainbow.jpeg" alt="Rainbow Flavors" class="rain"/>
+    </div>
+    
+        
+        </section>
+
+
+        <section class="pieceof">
+        <h1 class="okays">Healthy Juice Concentrates for All Kids to Enjoy</h1>
+        <img src="images/apjuice.png" alt="Juices" class="juice"/>
+        
+        <article class = "pennn"> We provide a variety of flavors that all kids can enjoy! All the juice concentrates that we sell are compliant with the USDA and FDA guidelines. You don't need to refrigerate the concentrates before or after opening them.
+        </article>
+      
+        <img src="images/kidsss.jpeg" alt="Kids" class="kids"/>
+        </section>
+
+        <section class="pieceofpi">
+        <h1 class="okay">Penny Juice Products Comply With All Requirements for CACFP, Subsidy, and Nutrition Set Forth by the FDA</h1>
+        <article class = "pennn">If you're running a childcare center and want to serve healthy beverages to kids every day, order quality juice concentrates from Penny Juice of America. 
+            
+            Our system is fast and easy to use. Penny Juice greatly reduces storage and handling requirements. 1 gallon pre-measured mix pitchers available to assure cost control and flavor consistency </article>
+
+            <img src="images/pofamer.png" alt="PennyofAmerica" class="pennyofa"/>
+
+            <p class = "pennn">Penny Juice of America is a Premium Quality 100% Fruit Juice Concentrate. We use only the highest quality fruit juices in our products.
+        </p>
+                <p class = "pennn">All our juice concentrates are made from 100% juice and do not contain any artificial colors.
+                
+                All our juice concentrates are sugar-free.
+            </p>
+            <p class = "pennn">
+                The ingredients in our concentrates are concentrated fruit juice, filtered water, malic acid, ascorbic acid (vitamin C), natural and artificial flavour, caramel. Sodium benzoate to maintain freshness.</p>
+        </section>
+        </section>
+        
+  
+  
+        
+</>
+)
+    }
+
+    export default Home;
