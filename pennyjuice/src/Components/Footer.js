10746bcdf14e9f3eb5f5596b4ed21dea30c27b26
vjaxel34 <92274553+vjaxel34@users.noreply.github.com>
2022-04-06 15:52:19 -0400

    Nav and Foot Complete

--- pennyjuice/src/Components/Footer.js
+++ pennyjuice/src/Components/Footer.js
@@ -6,10 +6,39 @@ import { AiOutlineInstagram } from "react-icons/ai";
 function Footer() {
   return (
     <>
-      <section className="main-footer">
-        <div>
-          
-        </div>
+      <section>
+        <section className="new-contact">
+          <div>
+            <h1>Contact Us</h1>
+            <p>Call: +1 (336) 333 333</p>
+            <p>Email: pennyjuice65@gmail.com</p>
+            <p>Mon to Fri 11 pm - 7 am</p>
+          </div>
+          <div>
+            <h1>Visit Us</h1>
+            <p>Envoy So. California</p>
+            <p>24 Honey, Ste</p>
+            <p>Building 2</p>
+          </div>
+          <div>
+            <h1>Social Media</h1>
+            <div>
+              <a href="twitter.com/lang=en">
+                <AiFillTwitterCircle /> pennyjuice@twitter.com
+              </a>
+            </div>
+            <div>
+              <a href="github.com">
+                <AiOutlineGithub /> pennyjuice34
+              </a>
+            </div>
+            <div>
+              <a href="instagram.com">
+                <AiOutlineInstagram /> penny_juice
+              </a>
+            </div>
+          </div>
+        </section>
         <div className="foot-content">
           <p className="p1">About Us</p>
           <p className="p2">Privacy Policy</p>
