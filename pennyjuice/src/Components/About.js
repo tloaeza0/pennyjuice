ddfec562751eb37b6b1203dfa84b0b3518af24a7
Giselle Guevara <giselleguevara@R2Hstudent.org>
2022-04-06 15:20:59 -0400

    update ainfprmation page

---
+++ pennyjuice/src/Components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="heroimgone-container">
+      <div className="heroimgone-text">
+          <h1>NO SUGAR ADDED! </h1>
+          <p>Get delicious and nutritious 100% fruit juice from us Today! We provide 100% blended fruit juice and containers specially made for kids and let's not forget that Penny Juice meets all the requirements of the FDA regarding subsidy and nutrition. </p>
+       </div>
+        </div>
+        <div className="heroimgtwo">
+      <div className="heroimgtwo-text">
+          <h1>Benifits </h1>
+          <p>You don't need to refrigerate the containers before or after opening them and making it one more minor problem to worry about. Penny Juice reduces the cost per ounce in every purchase. Each case contains six half-gallon bottles of different flavors.</p>
+       </div>
+        </div>
+        <div className="heroimgthree">
+      <div className="heroimgthree-text">
+          <h1>Love </h1>
+          <p>Kids love Penny Juice because it is available in various exciting flavors and colors!</p>
+       </div>
+        </div>
+        
+
+     
+    </>
+  );
+};
+export default About;
