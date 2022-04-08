5d7823df8e0a2603157f8a291f7012bb046930b2
Chioma Onukogu <conukogu1@gmail.com>
2022-04-08 11:47:28 -0400

    slider is now resizable

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import pic1 from './Components/Images/pic1.png'
 import Slider1 from './Components/Slider1';
 import Slider2 from "./Components/Slider2";
 import Slider3 from "./Components/Slider3";
+import Slider from "./Components/Slider";
 
 function App() {
   return (
     <div className="App">
-      <Slider1/>
-      {/* <Slider2 /> */}
-      {/* <Slider3 /> */}
+      <Slider/>
     </div>
   );
 }
