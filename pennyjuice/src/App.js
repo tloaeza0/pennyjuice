85e9c5bcdb184c685320c785bd66a193f0964339
vjaxel34 <92274553+vjaxel34@users.noreply.github.com>
2022-04-08 10:49:21 -0400

    modified App.js -Axel

--- pennyjuice/src/App.js
+++ pennyjuice/src/App.js
@@ -1,24 +1,15 @@
-import { Route, Routes} from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import ProductsPage from "./Pages/ProductsPage";
-import ContactPage from "./Pages/ContactPage";
-import OrderPage from './Pages/OrderPage'
+import { Route, Routes } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
-
 
 function App() {
   return (
     <div>
-      
       <NavBar />
       <main>
         <Routes>
-         <Route path='/' />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
           <Route path="/contact" element={<ContactPage />} />
-          <Route path="/order" element={<OrderPage />} />
         </Routes>
       </main>
       <Footer />
