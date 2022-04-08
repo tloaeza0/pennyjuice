a326fa94f1e452e9fb1ecd0506e89227e3dd746f
Chioma Onukogu <conukogu1@gmail.com>
2022-04-08 10:23:55 -0400

    all slider components mostly working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './main.css';
+import pic1 from './Components/Images/pic1.png'
+
+import Slider1 from './Components/Slider1';
+import Slider2 from "./Components/Slider2";
+import Slider3 from "./Components/Slider3";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider1/>
+      {/* <Slider2 /> */}
+      {/* <Slider3 /> */}
     </div>
   );
 }
