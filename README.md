ac37fa32f8d4910b3942c9d74cad93699c75aea8
Chioma Onukogu <conukogu1@gmail.com>
2022-04-11 14:04:06 -0400

    to revert

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# pennyjuice
-Redesign website
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './main.css';
+import pic1 from './Components/Images/pic1.png'
+
+import Slider1 from './Components/Slider1';
+import Slider2 from "./Components/Slider2";
+import Slider3 from "./Components/Slider3";
+import Slider from "./Components/Slider";
+
+function App() {
+  return (
+    <div className="App">
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
